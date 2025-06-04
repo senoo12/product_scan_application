afabc3427fce4199420653f47557809f7ee189ed
senoaji12 <pamungkas.seno11@gmail.com>
2025-06-04 11:54:31 +0700

    update setting database

--- backend/settings.py
+++ backend/settings.py
@@ -76,27 +76,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'seno11$db_scan_application',
-#         'USER': 'seno11',
-#         'PASSWORD': 'Passwordmd5*123',
-#         'HOST': 'seno11.mysql.pythonanywhere-services.com',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_scan_application',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'NAME': 'seno11$db_scan_application',
+        'USER': 'seno11',
+        'PASSWORD': 'Passwordmd5*123',
+        'HOST': 'seno11.mysql.pythonanywhere-services.com',
         'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'db_scan_application',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
